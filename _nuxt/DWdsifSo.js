fcefc7792d9526cf5d243fb3f228ea971b719b2d
顏劭軒 <dknick081@gmail.com>
2025-08-21 14:59:06 +0800

    feat:SSG

---
+++ _nuxt/DWdsifSo.js
@@ -0,0 +1,1 @@
+import{u as i}from"./BTC9fvUw.js";import{k as o}from"./weGZ5Ckw.js";const{width:e}=i(),c=()=>{const s=o(()=>e.value<640),t=o(()=>e.value<=1615);return{isMobile:s,isLessThen2XL:t}};export{c};
---
+++ _nuxt/Dyti4890.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CUKV1Opb.js";import"./VwCdnJSJ.js";import"./weGZ5Ckw.js";export{o as default};
